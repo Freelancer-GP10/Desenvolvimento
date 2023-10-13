4ad10f0f5107a2aae40f7474893ee03f221263b3
NauanaCoelho0627 <nauana.santos@sptech.school>
2023-10-13 00:05:28 -0300

    atualizações

--- web/src/Cadastro_Microempreendedor.jsx
+++ web/src/Cadastro_Microempreendedor.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./css/global.css";
 import "./css/img.css";
 import "./css/cadastro.css";
@@ -36,6 +35,9 @@ import imgLogo from "./img/logo.png"
 function CadastroMicroempreendedor2() {
     return (
         <>
+
+            <div className="backGradient"></div>
+            
             <section>
                 {/* Imagem */}
                 <div className="cont2"></div>
@@ -49,11 +51,9 @@ function CadastroMicroempreendedor2() {
                     <div className="form2">
 
                     <a href="./" className="a2">
-
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-                        </svg>
-                    
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                    </svg>
                     </a>
 
                         <form action="">
