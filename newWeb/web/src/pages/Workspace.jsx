7ae986a4a7ba1faa5266e64700c67995a466d86a
NauanaCoelho0627 <nauana.santos@sptech.school>
2023-11-16 00:20:17 -0300

    criação do campo de pesquisa

--- newWeb/web/src/pages/Workspace.jsx
+++ newWeb/web/src/pages/Workspace.jsx
@@ -3,6 +3,7 @@ import { PerfilComponente } from "../componentes/perfilComp"
 import "../css/service.css";
 import "../css/global.css";
 import { ServiceComponente2 } from "../componentes/serviceComponente";
+import { Pesquisa } from "../componentes/pesquisa";
 
 export function Workspace(){
     return(
@@ -12,6 +13,7 @@ export function Workspace(){
         <h2 className="h2-title2">Services</h2>
 
         <PerfilComponente />
+        <Pesquisa />
 
         <div className="conteudo">
             <ServiceComponente2 />
--- newWeb/web/src/pages/service.jsx
+++ newWeb/web/src/pages/service.jsx
@@ -3,6 +3,7 @@ import { PerfilComponente } from "../componentes/perfilComp"
 import "../css/service.css";
 import "../css/global.css";
 import { ServiceComponente } from "../componentes/serviceComponente";
+import { Pesquisa } from "../componentes/pesquisa";
 
 export function Service(){
     return(
@@ -12,6 +13,7 @@ export function Service(){
         <h2 className="h2-title2">Services</h2>
 
         <PerfilComponente />
+        <Pesquisa />
 
         <div className="conteudo">
             <ServiceComponente />
